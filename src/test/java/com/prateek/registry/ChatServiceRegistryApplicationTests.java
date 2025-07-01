920b5a8e4086d045640125378c05ba06de190d8f
Prateek Chaudhary <prateek.chaudhary@egallied.com>
2025-07-02 00:26:05 +0530

    registry initial commit

---
+++ src/test/java/com/prateek/registry/ChatServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prateek.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
